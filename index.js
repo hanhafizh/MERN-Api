fdbfcf67b4123436ac8efd0f93781a2fe461f649
hanhafizh <farhanhafizh770@gmail.com>
2023-07-02 01:31:26 +0700

    configurasi controllers, routes, & add body-parser

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+const productRoutes = require("./src/routes/products");
 
-app.use(() => {
-  console.log("hello server...");
-  console.log("nodemon successs...");
-  console.log("aman frennn...");
+app.use(bodyParser.json()); //type Json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
+
+app.use("/v1/customer", productRoutes);
 
 app.listen(4000);
