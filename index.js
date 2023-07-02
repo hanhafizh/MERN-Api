409ed95d3fbf098888bf6c4eddb02b054ef2f77e
hanhafizh <farhanhafizh770@gmail.com>
2023-07-02 21:08:40 +0700

    Setup MongoDB Model dengan Mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const authRoutes = require("./src/routes/auth");
@@ -27,4 +28,11 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-app.listen(4000);
+mongoose
+  .connect(
+    "mongodb+srv://farhan:hzdYWadNKnZk0497@cluster0.5j9jx7d.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(4000, () => console.log("Connection Succes"));
+  })
+  .catch((err) => console.log(err));
